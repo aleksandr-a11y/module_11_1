b8f0c097d316203f4f867d83cce4526c86624e93
ПК <@DESKTOP-H9D4HFD>
2024-11-13 21:05:33 +0700

    Добавьте файлы проекта.

---
+++ module_11_1.py
@@ -0,0 +1,29 @@
+﻿from PIL import Image, ImageOps # модуль оброботки изображения
+
+
+with Image.open("123.jpg") as im:
+    im.rotate(45).show() #поворачивает картинку на 45 градусов
+
+
+size = (100, 150)
+with Image.open("123.jpg") as im:
+    ImageOps.contain(im, size).save("imageops_contain.webp") #делает картинку маленькой
+    ImageOps.invert(im).save("321.jpg") # инвертирует картинку и сохроняет в файл
+
+import requests
+
+# Запрос данных с сайта
+response = requests.get('https://api.github.com')
+# Вывод ответа в консоль
+print(response.json())  # Печатает JSON-ответ от GitHub API
+print("\n код requests завершён \n")
+
+import numpy as np
+#Создаем массив с помощью библиотеки numpu
+
+arr = np.array([10, 20, 30, 40, 50])
+result = arr + 10
+print("Массив увеличенный на 10:", result)
+print(f'Минимальное значение массива: {result.min()}')
+print(f'Максимальное значение массива: {result.max()}')
+print(f'Сумма всех элементов массива: {result.sum()}')
